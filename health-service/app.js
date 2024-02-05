13c5e8339b22b6631ecbd4c09c1134e769a50442
Siddharth Khonde <siddharthkhonde.freedygo@gmail.com>
2024-02-05 15:10:24 -0500

    added colon

--- health-service/app.js
+++ health-service/app.js
@@ -8,5 +8,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var routes = require("./routes/routes.js")(app);
 
 var server = app.listen(8080, function () {
-  console.log("Listening on port %s...", server.address().port);
+  console.log("Listening on port: %s...", server.address().port);
 });
